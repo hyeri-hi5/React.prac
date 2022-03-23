60e96bbd2a528be6f4f0af6985feb59fb0e49697
hyeri-hi5 <merryk555@gmail.com>
2022-03-23 13:46:09 +0900

    기능 연결 및 최종 파일 생성

--- src/Rating.js
+++ src/Rating.js
@@ -1,83 +1,95 @@
-import React, {useState} from "react";
+import React, { Component, useState } from "react";
 import styled from "styled-components";
 import { useHistory } from "react-router-dom";
+import Update from "./Update";
 
-function Rating () {
+function Rating() {
+  const history = useHistory();
+  const day_count = Array.from({ length: 7 }, (v, i) => i);
+  const week_day = {
+    0: "일",
+    1: "월",
+    2: "화",
+    3: "수",
+    4: "목",
+    5: "금",
+    6: "토",
+  };
+  const day_text = Object.values(week_day);
 
-const history = useHistory();
-const day_count = Array.from({length: 7}, (v,i) => i);
-const week_day = {0: "일", 1: "월", 2: "화", 3: "수", 4: "목", 5: "금", 6: "토"}
-const day_text = Object.values(week_day)
-
-const day_rates = day_text.map((d, idx) => {
+  const day_rates = day_text.map((d, idx) => {
     return {
-      day: d, 
-      rate: Math.floor((Math.random() * 5) + 1)
-    }
-  })
+      day: d,
+      rate: Math.floor(Math.random() * 5 + 1),
+    };
+  });
 
-const 
-
-return (
+  return (
     <Wrap>
       <h1 style={{ textAlign: "center" }}>내 일주일은?</h1>
       {day_rates.map((x, i) => {
         return (
-            <Container key = {i}>
-                <p>
-                    {day_text[i]}
-                </p>
-                  {Array.from({ length: 5 }, (item, idx) => {
-                    return (
-                      <Circle style={{backgroundColor: idx < x.rate ? "orange" : "#ddd"}}/>
-                    );
-                  })}
-                      <Triangle onClick = {() => {
-                        history.push(`/rating/${day_text[i]}요일`)
-                      }}/>
-            </Container>  
-        )
+          <Container key={i}>
+            <p>{day_text[i]}</p>
+
+            {Array.from({ length: 5 }, (item, idx) => {
+              return (
+                <Circle
+                  style={{ backgroundColor: idx < x.rate ? "orange" : "#ddd" }}
+                />
+              );
+            })}
+            <Triangle
+              onClick={() => {
+                history.push(`/rating/${day_text[i]}요일`);
+              }}
+            />
+          </Container>
+        );
       })}
-            
     </Wrap>
-)    
-};
+  );
+}
 
 const Wrap = styled.div`
-max-width: 450px;
-width: 30vw;
-height: 80vh;
-margin: 40px auto;
-padding: 20px 0;
-border: 1px solid #ddd;
-border-radius: 5px;
+  max-width: 450px;
+  width: 30vw;
+  height: 80vh;
+  margin: 40px auto;
+  padding: 20px 0;
+  border: 1px solid #ddd;
+  border-radius: 5px;
 `;
 
 const Container = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-margin: 1rem 0;
-width: 100%;
-p {margin: 0 0.6em 0 0;} //네스팅 이렇게 쓰는거 아닌가..왜 안돼??????????????
-`; 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 1rem 0;
+  width: 100%;
+  p {
+    margin: 0 0.6em 0 0;
+    font-size: large;
+    font-weight: bold;
+  }
+`;
 
 const Circle = styled.div`
-width: 30px;
-height: 30px;
-border-radius: 30px;
-margin: 5px;
-`
+  width: 30px;
+  height: 30px;
+  border-radius: 30px;
+  margin: 5px;
+`;
 
 const Triangle = styled.div`
-border-color: purple;
-width: 0;
-height: 0;
-border-top: 1rem solid transparent;
-border-bottom: 1rem solid transparent;
-border-left: 1.6rem solid purple;
-color: purple;
-cursor: pointer;
-`
+  border-color: purple;
+  width: 0;
+  height: 0;
+  border-top: 1rem solid transparent;
+  border-bottom: 1rem solid transparent;
+  border-left: 1.6rem solid purple;
+  color: purple;
+  cursor: pointer;
+`;
 
 export default Rating;
--- src/Update.js
+++ src/Update.js
@@ -1,71 +1,95 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { useHistory} from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 
+function Update() {
+  const history = useHistory();
+  const [rate, setRate] = React.useState(0);
+  const params = useParams();
+  console.log(params);
 
-function Update () {
+  return (
+    <>
+      <Box>
+        
+          <h3 style={{ textAlign: "center" }}>
+            <Mini>{params.dayName}</Mini>평점 남기기</h3>
+        
 
-    const history = useHistory();
-    const [rate, setRate] = React.useState(0);
+        <RatingUpdate>
+          {Array.from({ length: 5 }, (item, idx) => {
+            return (
+              <Round
+                key={idx}
+                onClick={() => {
+                  setRate(idx + 1);
+                }}
+                style={{ backgroundColor: idx < rate ? "orange" : "#ddd" }}
+              />
+            );
+          })}
+        </RatingUpdate>
 
-    return (
-        <>
-          <Box>
-            <h3 style={{ textAlign: "center" }}>
-              무슨요일 평점 남기기
-            </h3>
-    
-            <RatingUpdate>
-              {Array.from({ length: 5 }, (item, idx) => {
-                return (
-                  <Round key={idx} onClick={() => {setRate(idx+1)}} style={{backgroundColor: idx < rate ? "orange" : "#ddd"}}/>
-                );
-              })}
-            </RatingUpdate>
-    
-            <Goback onClick={() => {history.push(`/`)}}>
-              평점 남기기
-            </Goback>
-          </Box>
-        </>
-      );
+        <Goback
+          onClick={() => {
+            history.push(`/`);
+          }}
+        >
+          평점 남기기
+        </Goback>
+      </Box>
+    </>
+  );
 }
 
 const Box = styled.div`
-max-width: 350px;
-width: 80vw;
-height: 90vh;
-margin: 5vh auto;
-padding: 5vh 5vw;
-border: 1px solid #ddd;
-box-sizing: border-box;
-border-radius: 5px;
+  max-width: 350px;
+  width: 80vw;
+  height: 90vh;
+  margin: 5vh auto;
+  padding: 5vh 5vw;
+  border: 1px solid #ddd;
+  box-sizing: border-box;
+  border-radius: 5px;
+  h3 {
+    display: flex;
+  }
+`;
+
+const Mini = styled.div`
+  color: #fff;
+  font-weight: 900;
+  background: orange;
+  padding: 0.2rem;
+  border-radius: 5px;
+  margin: 0 0.6rem 0 0; 
 `;
 
 const RatingUpdate = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-margin: 1rem 0;
-width: 100%;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 1rem 0;
+  width: 100%;
+`;
 
 const Round = styled.div`
-width: 30px;
-height: 30px;
-border-radius: 30px;
-margin: 5px;
-background-color: orange;
-`
+  width: 30px;
+  height: 30px;
+  border-radius: 30px;
+  margin: 5px;
+  background-color: orange;
+`;
 
 const Goback = styled.button`
-width: 100%;
-background-color: purple;
-border: none;
-border-radius: 5px;
-padding: 1rem;
-color: #fff;
-cursor: pointer;
-`    
+  width: 100%;
+  background-color: purple;
+  border: none;
+  border-radius: 5px;
+  padding: 1rem;
+  color: #fff;
+  font-size: medium;
+  cursor: pointer;
+`;
 
 export default Update;
